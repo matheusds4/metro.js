7b24c10167594ea18e51c286848525ebe09bbe2b
hydroperx <hydroperfox@gmail.com>
2025-11-07 12:20:35 -0300

    upd

--- src/components/PopoverMenu.tsx
+++ src/components/PopoverMenu.tsx
@@ -9,7 +9,7 @@ import { RTLContext } from "../layout/RTL";
 import { Theme, ThemeContext } from "../theme/Theme";
 import * as MathUtils from "../utils/MathUtils";
 import * as REMConvert from "../utils/REMConvert";
-import { COMMON_DELAY } from "../utils/Constants";
+import { COMMON_DELAY, MAXIMUM_Z_INDEX } from "../utils/Constants";
 
 /**
  * Either a popover menu or a context menu.
@@ -36,7 +36,10 @@ export function PopoverMenu(params: {
       }
       id={params.id}
       style={params.style}
-      ref={div}>
+      ref={div}
+      $backgroundColor={theme.colors.inputBackground}
+      $borderColor={theme.colors.inputBorder}
+      $foreground={theme.colors.foreground}>
 
       <div className="PopoverMenu-up-arrow"></div>
       <div className="PopoverMenu-content">
@@ -48,7 +51,52 @@ export function PopoverMenu(params: {
 }
 
 const Div = styled.div<{
-  //
+  $backgroundColor: string,
+  $borderColor: string,
+  $foreground: string,
 }> `
+  && {
+    display: inline-flex;
+    flex-direction: column;
+    position: fixed;
+    background: ${$ => $.$backgroundColor};
+    border: 0.15rem solid ${$ => $.$borderColor};
+    padding: ${REMConvert.pixels.remPlusUnit(6)} 0;
+    min-width: 12rem;
+    max-height: 30rem;
+    left: 0;
+    top: 0;
+    z-index: ${MAXIMUM_Z_INDEX};
+    visibility: hidden;
+    opacity: 0;
+  }
 
+  && > .PopoverMenu-up-arrow,
+  && > .PopoverMenu-down-arrow {
+    display: none;
+    color: ${$ => $.$foreground};
+    flex-direction: row;
+    justify-content: center;
+    height: ${REMConvert.pixels.remPlusUnit(10)};
+  }
+
+  && > .PopoverMenu-content {
+    display: flex;
+    flex-direction: column;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    flex-grow: 3;
+  }
+
+  /* Item*/
+  && > .PopoverMenu-content > .Item {
+  }
+
+  /* Indicator */
+  && > .PopoverMenu-content > .Item > span:nth-child(3) > .Indicator {
+  }
+
+  /* Separator */
+  && > .PopoverMenu-content > .Separator {
+  }
 `;
