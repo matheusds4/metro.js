f20b17e29ee23a2c3b9653d3be5bb20c8e6d16e1
hydroperx <hydroperfox@gmail.com>
2025-11-19 07:23:15 -0300

    upd

--- src/liveTiles/Detection.ts
+++ src/liveTiles/Detection.ts
@@ -26,6 +26,33 @@ export class Detection {
 
   //
   private _detect_now(): void {
-    //
+    let any_changes = false;
+    while (this._queue.length != 0) {
+      const node = this._queue.shift()!;
+      if (node.classList.contains(this.$._class_names.tile)) {
+        const this_changed = this._detect_tile(node as HTMLButtonElement);
+        any_changes ||= this_changed;
+      } else if (node.classList.contains(this.$._class_names.group)) {
+        const this_changed = this._detect_group(node);
+        any_changes ||= this_changed;
+      }
+    }
+    if (any_changes) {
+      this.$.rearrange();
+    }
+  }
+
+  // detect tile.
+  //
+  // return false if there were no changes detected.
+  private _detect_tile(node: HTMLButtonElement): boolean {
+    fixme();
+  }
+
+  // detect group.
+  //
+  // return false if there were no changes detected.
+  private _detect_group(node: HTMLElement): boolean {
+    fixme();
   }
 }
